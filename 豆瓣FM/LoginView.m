f93c799c5a3aaf40836b818f8ea6cd56a42bf15f
jiongjiongwang <wjsnh@aliyun.com>
2016-07-24 20:49:02 +0800

    (10)登录界面和用户界面设计完成

--- 豆瓣FM/LoginView.m
+++ 豆瓣FM/LoginView.m
@@ -8,6 +8,21 @@
 
 #import "LoginView.h"
 
+@interface LoginView()
+
+//(1)头像
+@property (nonatomic,weak)HeadImageView *headImageView;
+
+//(2)登录按钮
+@property (nonatomic,weak)UIButton *loginButton;
+
+//(3)注册按钮
+@property (nonatomic,weak)UIButton *registerButton;
+
+
+@end
+
+
 @implementation LoginView
 
 
@@ -16,11 +31,87 @@
 {
     if (self = [super init])
     {
-        self.backgroundColor = [UIColor greenColor];
+        //布局界面
+        [self setUpUI];
     }
-       
-    
     return self;
+}
+
+//布局界面
+-(void)setUpUI
+{
+    
+    self.backgroundColor = [UIColor lightGrayColor];
+    
+    //(1)头像
+    HeadImageView *headImageView = [[HeadImageView alloc] init];
+    self.headImageView = headImageView;
+    headImageView.image = [UIImage imageNamed:@"name"];
+    [self addSubview:headImageView];
+    
+    
+    
+    
+    //(2)登录按钮
+    UIButton *loginButton = [[UIButton alloc] init];
+    self.loginButton = loginButton;
+    [loginButton setTitle:@"登录" forState:UIControlStateNormal];
+    [loginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    loginButton.titleLabel.font = [UIFont systemFontOfSize:18];
+    
+    //添加登录的触发事件
+    [loginButton addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:loginButton];
+    
+    //(3)注册按钮
+    UIButton *registerButton = [[UIButton alloc] init];
+    self.registerButton = registerButton;
+    [registerButton setTitle:@"注册" forState:UIControlStateNormal];
+    [registerButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    registerButton.titleLabel.font = [UIFont systemFontOfSize:18];
+    //添加注册的触发事件
+    [registerButton addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:registerButton];
+}
+
+
+//重新布局界面，设置约束
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    
+    //(1)设置头像的约束
+    [self.headImageView makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.leading.equalTo(self.leading).offset(SCREEN_LEFT_WIDTH * 0.5 *0.8);
+        make.top.equalTo(self.top).offset(50);
+        make.height.width.equalTo(@75);
+        
+    }];
+    
+    //(2)设置登录按钮的约束
+    [self.loginButton makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(self.headImageView.bottom).offset(10);
+        make.right.equalTo(self.headImageView.centerX).offset(-20);
+    }];
+    
+    //(3)设置注册按钮的约束
+    [self.registerButton makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(self.headImageView.bottom).offset(10);
+        make.left.equalTo(self.headImageView.centerX).offset(20);
+    }];
+    
+}
+
+
+
+//登录/注册按钮
+-(void)clickButton:(UIButton *)btn
+{
+    NSLog(@"点击了登录/注册按钮");
 }
 
 
--- 豆瓣FM/UserView.m
+++ 豆瓣FM/UserView.m
@@ -8,6 +8,17 @@
 
 #import "UserView.h"
 
+@interface UserView()
+
+//(1)用户头像
+@property (nonatomic,weak)HeadImageView *headImageView;
+
+//(2)用户的用户名
+@property (nonatomic,weak)UILabel *nameLabel;
+
+
+@end
+
 @implementation UserView
 
 
@@ -16,11 +27,61 @@
 {
     if (self = [super init])
     {
-        self.backgroundColor = [UIColor blueColor];
+        //布局界面
+        [self setUpUI];
     }
     
     
     return self;
+}
+
+-(void)setUpUI
+{
+    
+    self.backgroundColor = [UIColor lightGrayColor];
+    
+    //(1)头像
+    HeadImageView *headImageView = [[HeadImageView alloc] init];
+    self.headImageView = headImageView;
+    headImageView.image = [UIImage imageNamed:@"name"];
+    [self addSubview:headImageView];
+    
+    
+    //(2)用户名
+    UILabel *nameLabel = [[UILabel alloc] init];
+    self.nameLabel = nameLabel;
+    nameLabel.text = @"用户名是啥是啥是啥hahahaha好好好好好到的";
+    nameLabel.font = [UIFont systemFontOfSize:15];
+    nameLabel.numberOfLines = 0;
+    [nameLabel setTextAlignment:NSTextAlignmentCenter];
+    //设置nameLabel的最大显示宽度
+    nameLabel.preferredMaxLayoutWidth = SCREEN_LEFT_WIDTH * 0.8;
+    [self addSubview:nameLabel];
+}
+
+//重新设置子控件的布局
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    
+    //(1)设置头像的约束
+    [self.headImageView makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.leading.equalTo(self.leading).offset(SCREEN_LEFT_WIDTH * 0.5 *0.8);
+        make.top.equalTo(self.top).offset(40);
+        make.height.width.equalTo(@75);
+        
+    }];
+    
+    
+    //(2)设置用户名的约束
+    [self.nameLabel makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(self.headImageView.bottom).offset(10);
+        make.centerX.equalTo(self.headImageView.centerX);
+    }];
+    
 }
 
 
